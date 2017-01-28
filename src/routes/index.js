2f9ccf71d304201bbaecb0a0fb673f27474576f4
k.tashiro <tashiro@lepra.jp>
2017-01-28 22:53:42 +0900

    fontawesome追加

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function(req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-/* GET home page. */
+/* Mabo login form */
 router.get('/mabo', function(req, res, next) {
     res.render('mabo/index', {title: 'Mabo'});
+});
+
+/* Mabo scenario */
+router.get('/mabo/scenario', function(req, res, next) {
+    res.render('mabo/scenario/index', {title: 'Mabo'});
 });
 
 module.exports = router;
