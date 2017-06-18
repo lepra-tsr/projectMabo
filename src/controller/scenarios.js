0d4433ddcb7a323a5524afa10323f3e0b9a49c2b
k.tashiro <tashiro@lepra.jp>
2017-06-19 01:51:58 +0900

    シナリオ一覧画面→プレイ画面のつなぎ込み

--- src/controller/scenarios.js
+++ src/controller/scenarios.js
@@ -8,11 +8,45 @@ let fs          = require('fs');
 let def         = require('../mabo_modules/def');
 const mongoPath = def.mongoPath;
 
+/*
+ * 画面API
+ */
+router.get('/list', function(req, res, next) {
+    /*
+     * シナリオ一覧画面
+     */
+    res.render('scenarios/list', {title: 'シナリオ一覧'});
+});
 
+router.get('/:scenarioId', function(req, res, next) {
+    /*
+     * シナリオプレイ画面
+     */
+    let scenarioId = new ObjectId(req.params.scenarioId);
+    
+    mc.connect(mongoPath, function(error, db) {
+        assert.equal(null, error);
+        db.collection('scenarios').find({_id: {$eq: scenarioId}}, {name: 1})
+            .toArray(function(error, doc) {
+                if (doc.length === 0) {
+                    res.render('scenarios/list', {title: 'シナリオ一覧'});
+                }
+                
+                res.render('scenarios/playGround', {
+                    title     : `『${doc[0].name}』`,
+                    scenarioId: req.params.scenarioId
+                })
+            })
+    });
+});
+
+/*
+ * Ajaxで叩く用
+ */
 router.get('', function(req, res, next) {
     
     /*
-     * シナリオ一覧画面
+     * シナリオ一覧取得
      */
     mc.connect(mongoPath, function(error, db) {
         assert.equal(null, error);
@@ -50,6 +84,98 @@ router.get('', function(req, res, next) {
                         return false;
                     });
             });
+    });
+});
+
+router.post('', function(req, res, next) {
+    
+    let name       = req.body.scenarioName;
+    let passPhrase = req.body.passPhrase;
+    let synopsis   = req.body.synopsis;
+    
+    let error = validateScenarioInfo(name, passPhrase, synopsis);
+    
+    if (error) {
+        res.status(422);
+        res.send('シナリオの登録内容にエラーがあったようです。');
+        return false;
+    }
+    
+    /*
+     * 部屋の作成
+     */
+    mc.connect(mongoPath, function(error, db) {
+        assert.equal(null, error);
+        let document = {
+            name      : name,
+            passPhrase: passPhrase,
+            synopsis  : synopsis,
+            closed    : false
+        };
+        
+        db.collection('scenarios').insertOne(document, function(error, db) {
+            if (error) {
+                res.status(500);
+                res.send('シナリオの作成に失敗しました。')
+            }
+            res.status(200);
+            res.send();
+        })
+    })
+    
+});
+
+router.patch('', function(req, res, next) {
+    
+    /*
+     * シナリオ設定内容変更用のエンドポイント
+     */
+    let scenarioId = new ObjectId(req.body.scenarioId);
+    let name       = req.body.scenarioName;
+    let passPhrase = req.body.passPhrase;
+    let synopsis   = req.body.synopsis;
+    let error      = validateScenarioInfo(name, passPhrase, synopsis);
+    
+    if (error) {
+        res.status(422);
+        res.send('シナリオの登録内容にエラーがあったようです。');
+        return false;
+    }
+    
+    mc.connect(mongoPath, function(error, db) {
+        assert.equal(error, null);
+        
+        db.collection('scenarios').find({_id: {$eq: scenarioId}})
+            .toArray(function(error, doc) {
+                /*
+                 * パスフレーズ検証
+                 */
+                if (doc[0].passPhrase !== passPhrase) {
+                    res.status(400);
+                    res.send('パスフレーズが違います。');
+                    return false;
+                }
+                
+                let document = {
+                    _id       : scenarioId,
+                    name      : name,
+                    passPhrase: passPhrase,
+                    synopsis  : synopsis,
+                    closed    : false
+                };
+                db.collection('scenarios')
+                    .updateOne({_id: {$eq: scenarioId}}, document, {upsert: true}, function(error) {
+                        if (error) {
+                            res.status(500);
+                            res.send('シナリオ設定に失敗しました。');
+                            return false;
+                        } else {
+                            res.status(200);
+                            res.send();
+                            return false;
+                        }
+                    })
+            })
     });
 });
 
@@ -91,60 +217,6 @@ router.patch('/close', function(req, res, next) {
     });
 });
 
-router.patch('', function(req, res, next) {
-    
-    /*
-     * シナリオ設定内容変更用のエンドポイント
-     */
-    let scenarioId = new ObjectId(req.body.scenarioId);
-    let name       = req.body.scenarioName;
-    let passPhrase = req.body.passPhrase;
-    let synopsis   = req.body.synopsis;
-    let error      = validateScenarioInfo(name, passPhrase, synopsis);
-    
-    if (error) {
-        res.status(422);
-        res.send('シナリオの登録内容にエラーがあったようです。');
-        return false;
-    }
-    
-    mc.connect(mongoPath, function(error, db) {
-        assert.equal(error, null);
-    
-        db.collection('scenarios').find({_id: {$eq: scenarioId}})
-            .toArray(function(error, doc) {
-                /*
-                 * パスフレーズ検証
-                 */
-                if (doc[0].passPhrase !== passPhrase) {
-                    res.status(400);
-                    res.send('パスフレーズが違います。');
-                    return false;
-                }
-                
-                let document = {
-                    _id       : scenarioId,
-                    name      : name,
-                    passPhrase: passPhrase,
-                    synopsis  : synopsis,
-                    closed    : false
-                };
-                db.collection('scenarios')
-                    .updateOne({_id: {$eq: scenarioId}}, document, {upsert: true}, function(error) {
-                        if (error) {
-                            res.status(500);
-                            res.send('シナリオ設定に失敗しました。');
-                            return false;
-                        } else {
-                            res.status(200);
-                            res.send();
-                            return false;
-                        }
-                    })
-            })
-    });
-});
-
 function validateScenarioInfo(name, passPhrase, synopsis) {
     /*
      * HTMLエンコードで文字数が膨らむため、厳密な文字数バリデーションは行わない
@@ -155,52 +227,5 @@ function validateScenarioInfo(name, passPhrase, synopsis) {
     err     = err || (typeof synopsis !== 'string' || synopsis.length > 2000);
     return err;
 }
-
-router.post('', function(req, res, next) {
-    
-    let name       = req.body.scenarioName;
-    let passPhrase = req.body.passPhrase;
-    let synopsis   = req.body.synopsis;
-    
-    let error = validateScenarioInfo(name, passPhrase, synopsis);
-    
-    if (error) {
-        res.status(422);
-        res.send('シナリオの登録内容にエラーがあったようです。');
-        return false;
-    }
-    
-    /*
-     * 部屋の作成
-     */
-    mc.connect(mongoPath, function(error, db) {
-        assert.equal(null, error);
-        let document = {
-            name      : name,
-            passPhrase: passPhrase,
-            synopsis  : synopsis,
-            closed    : false
-        };
-    
-        db.collection('scenarios').insertOne(document, function(error, db) {
-            if (error) {
-                res.status(500);
-                res.send('シナリオの作成に失敗しました。')
-            }
-            res.status(200);
-            res.send();
-        })
-    })
-    
-});
-
-router.get('/list', function(req, res, next) {
-    
-    /*
-     * シナリオ一覧画面
-     */
-    res.render('scenarios/list', {title: 'シナリオ一覧'});
-});
-
 
 module.exports = router;
