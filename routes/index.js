ab6ff6400930e9d867a1842940cf8d3769d695b8
tashiro <tsrmix@gmail.com>
2018-07-16 15:17:42 +0900

    impl base database connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const user = encodeURIComponent('mabo');
+const pwd = encodeURIComponent('azatoth');
+const dbName = 'mabo';
+const uri = `mongodb://${user}:${pwd}@52.194.141.20:27017/${dbName}`;
+
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Mabo'});
+  res.render('index', { title: 'Mabo' });
+});
+
+router.get('/db', (req, res, next) => {
+  MongoClient.connect(uri, { useNewUrlParser: true }, (e, client) => {
+    if (e) {
+      throw e;
+    }
+    const db = client.db('mabo');
+    db.collection('connection').find().toArray((e, docs) => {
+      res.status(200).send(docs);
+    });
+  });
 });
 
 module.exports = router;
