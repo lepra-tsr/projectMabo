46c351f95d2a0ac8f356e00a9e6818d43db3d798
tsr <tsrmix@gmail.com>
2018-07-24 17:03:00 +0900

    add description sample

--- src/schema/mabo.ts
+++ src/schema/mabo.ts
@@ -6,32 +6,39 @@ const {
   GraphQLInt,
   GraphQLString,
 } = require('graphql');
-// const {roomResolver} = require('./roomResolver');
-
-// export const schema = buildSchema(`
-//   type Query {
-//     room(id: Int title: String): [Room]
-//   }
-//
-//   type Room {
-//     id: Int
-//     title: String
-//   }
-// `);
 
 const Room = new GraphQLObjectType({
   name: 'Room',
+  description: 'schema room description',
   fields: () => ({
-    id: {type: GraphQLInt, resolve: (room) => room.id || 999},
-    title: {type: GraphQLString, resolve: (room) => room.title || '999'},
-    password: {type: GraphQLString, resolve: (room) => room.password || '999'},
+    id: {
+      type: GraphQLInt,
+      resolve: (room) => room.id || 999,
+      description: 'id \'s description'
+    },
+    title: {
+      type: GraphQLString,
+      resolve: (room) => room.title || '999',
+      description: 'title \'s description'
+    },
+    description: {
+      type: GraphQLString,
+      resolve: (room) => room.description || 'description sample',
+      description: 'description \'s description'
+    },
+    password: {
+      type: GraphQLString,
+      resolve: (room) => room.password || '999',
+      description: 'password \'s description'
+    },
   })
 });
 
+const loremJp = '複素数体であれば、任意のCM-タイプの A は、実際、数体である定義体（英語版）(field of definition)を持っている。自己準同型環の可能なタイプは、対合（ロサチの対合（英語版）(Rosati involution）をもつ環として既に分類されていて、CM-タイプのアーベル多様体の分類を導き出す。楕円曲線と同じような方法でCM-タイプの多様体を構成するには、Cd の中の格子 Λ から始め、アーベル多様体のリーマンの関係式（英語版）(Riemann relations)を考えに入れる必要がある。 CM-タイプ(CM-type)は、単位元での A の正則接空間上にある EndQ(A) の（最大）可換部分環 L の作用を記述したものである。単純な種類のスペクトル理論が適応され、L が固有ベクトルの基底を通して作用することを示すことができる。言い換えると、L は A の正則ベクトル場の上の対角行列を通した作用を持っている。L 自体がある複数の体の積というよりも数体であるという単純な場合には、CM-タイプは L の複素埋め込み（英語版）(complex embedding)のリストである。複素共役をなすペアとして、2d 個の複素埋め込みがあり、CM-タイプは各々のペアのから一つを選択する。そのようなCM-タイプの全てが実現されることが知られている。';
 const RoomList = [
-  {id: 1, title: 'title1', password: 'password1'},
-  {id: 2, title: 'title2', password: 'password2'},
-  {id: 3, title: 'title3', password: 'password3'},
+  { id: 1, title: 'title1', description: loremJp, password: 'password1' },
+  { id: 2, title: 'title2', description: loremJp, password: 'password2' },
+  { id: 3, title: 'title3', description: loremJp, password: 'password3' },
 ];
 
 /* 問い合わせ時のroomに対応 */
@@ -41,6 +48,7 @@ const Query = new GraphQLObjectType({
   fields: {
     room: {
       type: new GraphQLList(Room),
+      description: 'query room description',
       args: {
         id: {
           type: GraphQLInt,
@@ -48,8 +56,11 @@ const Query = new GraphQLObjectType({
         }
       },
       resolve: (...args) => {
-        console.log(args[0]); // @DELETEME
-        console.log(args[1]); // @DELETEME
+        const [, { id }] = args;
+
+        if (id) {
+          return RoomList.filter(r => r.id === id);
+        }
         return RoomList;
       },
     },
@@ -65,10 +76,3 @@ const Query = new GraphQLObjectType({
 export const schema = new GraphQLSchema({
   query: Query,
 });
-
-
-// export const resolver = {
-//   room: roomResolver,
-//   // chat,
-//   // user,
-// };
