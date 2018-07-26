fe86c09cb212f24b528e45b77ccbc32c5ff4d2f5
tsr <tsrmix@gmail.com>
2018-07-26 10:47:42 +0900

    get token sample

--- src/schema/query/token/query.ts
+++ src/schema/query/token/query.ts
@@ -1,8 +1,13 @@
 const {
   GraphQLNonNull,
   GraphQLInt,
+  GraphQLString,
 } = require('graphql');
-const {Token} = require('../../model/Token/type');
+const { Token } = require('../../model/Token/type');
+const crypto = require('crypto');
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
+const salt = env['SHA256_SALT'];
 
 export const tokenQuery = {
   type: Token,
@@ -11,10 +16,23 @@ export const tokenQuery = {
     id: {
       type: new GraphQLNonNull(GraphQLInt),
       description: 'token id',
+    },
+    roomId: {
+      type: new GraphQLNonNull(GraphQLInt),
+      description: 'room id',
+    },
+    password: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'password',
     }
   },
   resolve: (...args) => {
-    const [/* source */, {id}, /* context */] = args;
-    return {id: id, hash: 'aaa'}
+    const [/* source */, { id, roomId, password }, /* context */] = args;
+
+    const hash = crypto.createHmac('sha256', salt);
+    hash.update(password);
+    const sha256 = hash.digest('hex');
+
+    return { id, roomId, hash: sha256 }
   },
 };
