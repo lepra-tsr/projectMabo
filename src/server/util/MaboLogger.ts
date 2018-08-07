85a5e50d756c9a2ecac7e24d8154b817e6571355
k.tashiro <tashiro@lepra.jp>
2018-08-07 16:16:27 +0900

    logger

---
+++ src/server/util/MaboLogger.ts
@@ -0,0 +1,29 @@
+const log4js = require('log4js');
+log4js.configure({
+  appenders:
+  {
+    console: {
+      type:'console'
+    },
+    everything: {
+      type: 'dateFile',
+      filename: 'logs/whole_log',
+      pattern: '_yyyy_MM_dd.log',
+      daysToKeep: 21,
+    },
+    socket: {
+      type: 'dateFile',
+      filename: 'logs/socket_log',
+      pattern: '_yyyy_MM_dd.log',
+      daysToKeep: 21,
+    },
+  },
+  categories: {
+    default: { appenders: ['everything','console'], level: 'debug' },
+    socket: { appenders: ['everything','console','socket'], level: 'debug' },
+  },
+  replaceConsole: true
+})
+export const lg = log4js.getLogger();
+export const slg = log4js.getLogger('socket');
+lg.level = 'debug';
