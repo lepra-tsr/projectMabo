66a4fce0374c39f05eece70364c03dcb2b9ae814
tsr <tsrmix@gmail.com>
2018-08-12 03:34:55 +0900

    pluck log component

---
+++ src/client/Logs.tsx
@@ -0,0 +1,87 @@
+"use strict";
+import * as React from "react";
+import { Listener } from "./Listener";
+import { Connection } from "./socketeer/Connection";
+import { GraphCaller } from "./GraphCaller";
+
+interface ILogsState {
+  logs: {
+    id: string,
+    socketId: string,
+    userName: string,
+    channelId: string,
+    avatarId: string,
+    content: string,
+    faceId: string,
+  }[],
+}
+
+export class Logs extends React.Component<{}, ILogsState> {
+  static instance?: Logs;
+  constructor(props) {
+    super(props);
+
+    if (typeof Logs.instance === 'object') {
+      return Logs.instance;
+    }
+    Logs.instance = this;
+
+    this.state = {
+      logs: [],
+    };
+  }
+
+  componentDidMount() {
+    Listener.on('chatText', this.chatTextHandler.bind(this));
+    this.loadAllChats();
+  }
+
+  async loadAllChats() {
+    const query = `
+    query($roomId:String!){
+      chat(roomId:$roomId) {
+        _id
+        roomId
+        socketId
+        userName
+        channelId
+        avatarId
+        content
+        faceId
+      }
+    }`;
+    const variables = { roomId: Connection.roomId };
+    try {
+      const { data } = await GraphCaller.call(query, variables);
+      const { chat } = data;
+      const logs = chat.map((c) => ({
+        id: c._id,
+        socketId: c.socketId,
+        userName: c.userName,
+        channelId: c.channelId,
+        avatarId: c.avatarId,
+        content: c.content,
+        faceId: c.faceId,
+      }));
+      this.setState({ logs });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  chatTextHandler(chat) {
+    const logs = this.state.logs.slice();
+    logs.push(chat);
+    this.setState({ logs });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>logs</h4>
+        {this.state.logs
+          .map((l) => (<p key={l.id}>{l.userName}({l.socketId}),{l.content}</p>))}
+      </div>
+    )
+  }
+}
--- src/client/SessionContainer.tsx
+++ src/client/SessionContainer.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { ChangeEvent } from 'react';
 import { UserNameDialog } from "./UserNameDialog";
+import { Logs } from "./Logs";
 import { Listener } from "./Listener";
 import { Connection } from "./socketeer/Connection";
 import { GraphCaller } from "./GraphCaller";
@@ -10,15 +11,17 @@ interface ISessionContainerState {
   userName: string;
   users: { id: string, name: string, socketId: string }[];
   inputText: string;
-  logs: {
+  inputChannel: string,
+  channel: string,
+  channels: {
     id: string,
-    socketId: string,
-    userName: string,
-    channelId: string,
-    avatarId: string,
-    content: string,
-    faceId: string,
+    name: string,
   }[],
+  pickers: {
+    id: string,
+    name: string,
+    enabled: boolean,
+  }[]
 }
 
 export class SessionContainer extends React.Component<{}, ISessionContainerState> {
@@ -35,58 +38,94 @@ export class SessionContainer extends React.Component<{}, ISessionContainerState
       userName: 'デフォルト',
       users: [],
       inputText: '',
-      logs: [],
+      inputChannel: '',
+      channel: '',
+      channels: [],
+      pickers: [],
     };
   }
 
   componentDidMount() {
     Listener.on('roomUserInfo', this.roomUserInfoHandler.bind(this));
-    Listener.on('chatText', this.chatTextHandler.bind(this));
-    this.loadAllChats();
+    Listener.on('channelInfo', this.channelInfoHandler.bind(this));
+    this.loadAllChannels();
+    this.loadAllpickers();
   }
 
-  async  loadAllChats() {
+  async loadAllChannels() {
     const query = `
     query($roomId:String!){
-      chat(roomId:$roomId) {
+      channel(roomId:$roomId) {
         _id
         roomId
-        socketId
-        userName
-        channelId
-        avatarId
-        content
-        faceId
+        name
       }
     }`;
     const variables = { roomId: Connection.roomId };
     try {
       const { data } = await GraphCaller.call(query, variables);
-      const { chat } = data;
-      const logs = chat.map((c) => ({
+      const { channel: channelResult } = data
+      const channels = channelResult.map((c) => ({
         id: c._id,
-        socketId: c.socketId,
-        userName: c.userName,
-        channelId: c.channelId,
-        avatarId: c.avatarId,
-        content: c.content,
-        faceId: c.faceId,
+        name: c.name,
       }));
-      this.setState({ logs });
-    } catch (e) {
+      const channel = (this.state.channel === '') ? channels[0].id : this.state.channel;
+      this.setState({ channel, channels })
+    }
+    catch (e) {
       console.error(e);
     }
   }
 
-  chatTextHandler(chat) {
-    const logs = this.state.logs.slice();
-    logs.push(chat);
-    this.setState({ logs });
+  async loadAllpickers() {
+    const query = `
+    query($roomId:String!){
+      channel(roomId:$roomId) {
+        _id
+        roomId
+        name
+      }       
+    }`;
+    const variables = { roomId: Connection.roomId };
+    try {
+      const { data } = await GraphCaller.call(query, variables);
+      const { channel } = data
+      /* merging */
+      const pickers: { id: string, name: string, enabled: boolean }[] = [];
+      for (let i_c = 0; i_c < channel.length; i_c++) {
+        const c = channel[i_c];
+        let isExist = false;
+        for (let i_p = 0; i_p < this.state.pickers.length; i_p++) {
+          const p = this.state.pickers[i_p];
+          if (c._id === p.id) {
+            isExist = true;
+            break;
+          }
+        }
+        if (isExist) {
+          continue;
+        }
+        const picker: { id: string, name: string, enabled: boolean } = {
+          id: c._id,
+          name: c.name,
+          enabled: true,
+        }
+        pickers.push(picker);
+      }
+      this.setState({ pickers })
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
 
   roomUserInfoHandler(users) {
     const newUsers = [].concat(users);
     this.setState({ users: newUsers });
+  }
+
+  channelInfoHandler(channels) {
+    this.setState({ channels, pickers: channels })
   }
 
   static getUserName() {
@@ -97,30 +136,102 @@ export class SessionContainer extends React.Component<{}, ISessionContainerState
 
   render() {
     return (
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div>
-          <h4>Connection.userName:</h4>
-          <p>{this.state.userName}</p>
+          <div>
+            <h4>Connection.userName:</h4>
+            <p>{this.state.userName}</p>
+          </div>
+          <div>
+            <h4>users</h4>
+            {this.state.users
+              .map((u) => {
+                return (<p key={u.id}>{u.id}, {u.name}, {u.socketId}</p>)
+              })
+            }
+          </div>
+          <div>
+            <textarea onKeyUp={this.onKeyUpTextAreaHandler.bind(this)} />
+            <input type="button" value="send" onClick={this.onClickSendButtonHandler.bind(this)} />
+            <p>inputText:{this.state.inputText}</p>
+          </div>
+          <div>
+            <h5>channel</h5>
+            <select value={this.state.channel}
+              onChange={this.onChangeChannelSelectorHandler.bind(this)}>
+              {this.state.channels
+                .map((c) => (<option value={c.id} key={c.id}>{c.name}</option>))}
+            </select>
+            <input type="form" onKeyUp={this.onKeyUpChannelNameInputHandler.bind(this)} />
+            <input type="button" value="add channel" onClick={this.onClickAddChannelHandler.bind(this)} />
+            <p>{this.state.channel}</p>
+          </div>
+          <div>
+            <h5>picker</h5>
+            {this.state.pickers.map((p) => {
+              return (
+                <label key={p.id}>
+                  <span>{p.name}</span>
+                  <input key={p.id} type="checkbox" checked={p.enabled}
+                    onChange={this.onChangePickerInputHandler.bind(this, p.id)}
+                  /><br />
+                </label>
+              )
+            })}
+            <p>{this.state.pickers.filter(p => p.enabled).map(p => p.name).join(',')}</p>
+          </div>
         </div>
-        <div>
-          <h4>users</h4>
-          {this.state.users
-            .map((u) => {
-              return (<p key={u.id}>{u.id}, {u.name}, {u.socketId}</p>)
-            })
-          }
-        </div>
-        <div>
-          <h4>logs</h4>
-          <textarea onKeyUp={this.onKeyUpTextAreaHandler.bind(this)} />
-          <input type="button" value="send" onClick={this.onClickSendButtonHandler.bind(this)} />
-          <p>inputText:{this.state.inputText}</p>
-          {this.state.logs
-            .map((l) => (<p key={l.id}>{l.userName}({l.socketId}),{l.content}</p>))}
-        </div>
+        <Logs />
         <UserNameDialog />
       </div>
     )
+  }
+
+  onChangePickerInputHandler(pickerId) {
+    const pickers = this.state.pickers.slice();
+    for (let i_p = 0; i_p < pickers.length; i_p++) {
+      const p = pickers[i_p];
+      if (p.id === pickerId) {
+        p.enabled = !p.enabled;
+        break;
+      }
+    }
+    this.setState({ pickers });
+  }
+
+  onChangeChannelSelectorHandler(e: ChangeEvent<HTMLSelectElement>) {
+    const { currentTarget: target } = e;
+    if (target instanceof HTMLSelectElement) {
+      const { value: channel } = target;
+      this.setState({ channel });
+    }
+  }
+
+  onKeyUpChannelNameInputHandler(e: ChangeEvent<HTMLInputElement>) {
+    const { currentTarget: target } = e;
+    if (target instanceof HTMLInputElement) {
+      const { value: inputChannel } = target;
+      this.setState({ inputChannel });
+    }
+  }
+
+  async  onClickAddChannelHandler() {
+    const mutation = `
+    mutation($roomId:String! $name:String!) {
+      createChannel(
+        roomId: $roomId
+        name: $name
+      ){
+        _id
+        roomId
+        name
+      }
+    }`;
+    const variables = {
+      roomId: Connection.roomId,
+      name: this.state.inputChannel,
+    };
+    await GraphCaller.call(mutation, variables);
   }
 
   onKeyUpTextAreaHandler(e: ChangeEvent<HTMLTextAreaElement>) {
@@ -170,7 +281,6 @@ export class SessionContainer extends React.Component<{}, ISessionContainerState
       content: this.state.inputText,
       faceId: '012345678901234567890123'
     }
-    const json = await GraphCaller.call(mutation, variables);
-    console.log(json);
+    await GraphCaller.call(mutation, variables);
   }
 }
