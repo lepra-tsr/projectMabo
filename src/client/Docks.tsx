d9d8c6d75dc22975367885a3a74a2cb07c85ab2a
k.tashiro <tashiro@lepra.jp>
2018-08-24 15:46:44 +0900

    merge Docks and Dock

--- src/client/Docks.tsx
+++ src/client/Docks.tsx
@@ -6,14 +6,29 @@ import Rnd from 'react-rnd';
 const IPSUM = 'クーロンの法則は1785年から89年にかけて発見されたが、それまでの電磁気学（確立していないがそれに関する研究）は、かなり曖昧で定性的なものであった。\n電磁気学は、1600年にギルバートはコハクが摩擦でものを引きつける現象から、物質を電気性物質、非電気性物質として区別したことに始まり、1640年にはゲーリケによって放電が確認された。\n18世紀に入った1729年にグレイが金属が電気的性質を伝えることを発見し、その作用を起こす存在を電気と名付けた。彼はギルバートの電気性物質の区別を、電気を導く物質として導体、電気を伝えない物質を不導体と分類した。1733年、デュ・フェが摩擦によって生じる電気には二つの性質があり、同種間では反発し、異種間では引き合うこと、そして異種の電気を有する物質どうしを接触させると中和して電気的作用を示さなくなることを発見した。1746年にはライデン瓶が発明され、電気を蓄える技術を手に入れた。1750年には検電器が発明され、これらからフランクリンが電気にプラスとマイナスの区別をつけることでデュ・フェの現象を説明した。\nフランクリンの手紙に示唆されて、プリーストリーは1766年に中空の金属容器を帯電させ、内部の空気中に電気力が働かないことを示し、重力との類推から電気力が距離の2乗に反比例すると予想した[1][2]。1769年にジョン・ロビソン(John Robison)は実験により同種電荷の斥力は距離の2.06乗に反比例し、異種電荷の引力は距離の2以下の累乗に反比例することを見いだした。しかしこの結果は1803年まで公表されなかった[3]。1773年にイギリスのキャヴェンディッシュは同心にした2個の金属球の外球を帯電させ、その二つを帯電させたときに内球に電気が移らないことから逆二乗の法則を導き出した。これはまさにクーロンの法則であり、クーロンよりも早く、しかも高い精度で求めていた。しかし、彼は研究資料を机にしまい込んで発表しなかったためにおよそ100年の間公表されなかった。';
 // const IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
+/*
+ * 機能要件メモ
+ * 
+ * 表示(使いざま)
+ * ✔ 削除
+ * 上下フィット
+ * ✔ ドラッグで移動……ヘッダ 
+ * ✔ リサイズ
+ * 位置初期化API
+ * ✔ 重ね順(一番上とそれ以外)
+ * ✔ フォーカス時の重ね順変更
+ */
 interface IDocks {
   items: {
     key: number;
     zIndex: number;
     title: string;
     content: JSX.Element | string;
+    size?: { width: number, height: number };
+    position?: { x: number, y: number };
     default?: { x: number, y: number, width?: number, height?: number };
-    onClose: (key: number) => void;
+    onClose: () => void;
+    onBubbleUp: () => void;
   }[]
 }
 export default class Docks extends React.Component<{}, IDocks> {
@@ -27,13 +42,15 @@ export default class Docks extends React.Component<{}, IDocks> {
 
   addItem() {
     const { items } = this.state;
+    const key = this.index;
     const newItem = {
-      key: this.index,
+      key,
+      zIndex: items.length + 1,
       title: 'a',
       content: IPSUM,
-      zIndex: 0,
       default: { x: 50, y: 50, width: 300, height: 200 },
-      onClose: this.removeItem.bind(this),
+      onClose: this.removeItem.bind(this, key),
+      onBubbleUp: this.bubbleUp.bind(this, key),
     };
     items.push(newItem);
     this.setState({ items });
@@ -52,6 +69,20 @@ export default class Docks extends React.Component<{}, IDocks> {
     this.setState({ items });
   }
 
+  bubbleUp(key) {
+    const { items } = this.state;
+    items.sort((x, y) => {
+      const xZ = x.zIndex;
+      const yZ = y.zIndex;
+      return (xZ > yZ) ? 1 : -1;
+    })
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      item.zIndex = (item.key === key) ? items.length + 1 : i;
+    }
+    this.setState({ items });
+  }
+
   render() {
     return (
       <div>
@@ -63,86 +94,52 @@ export default class Docks extends React.Component<{}, IDocks> {
 
   renderDock() {
     const { items } = this.state
-    return (
-      <div>
-        {items.map((i) => {
-          return <Dock key={i.key} {...i} onClose={this.removeItem.bind(this, i.key)} />
-        })}
-      </div>
-    )
-  }
-}
+    const elList: JSX.Element[] = [];
 
-/*
- * 機能要件メモ
- * 
- * 表示
- * ✔ 削除
- * 上下フィット
- * ✔ ドラッグで移動……ヘッダ 
- * ✔ リサイズ
- * 位置初期化API
- * 重ね順(一番上とそれ以外)
- * フォーカス時の重ね順変更
- */
-interface IDockProps {
-  content: string | JSX.Element;
-  title: string;
-  zIndex: number;
-  default?: { x: number, y: number, width?: number, height?: number };
-  onClose: () => void;
-}
-interface IDockState {
-}
-class Dock extends React.Component<IDockProps, IDockState> {
-  rnd;
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-
-  render() {
-    const s: { [key: string]: CSSProperties } = {
-      rnd: {
-        border: '1px silver dashed',
-        backgroundColor: 'lightblue',
-        overflowX: 'hidden',
-        overflowY: 'hidden',
-        zIndex: this.props.zIndex,
-      },
-      h: {
-        border: '1px lightgray dashed',
-        backgroundColor: 'ghostwhite',
-        display: 'flex',
-      },
-      b: {
-        backgroundColor: 'white',
-        whiteSpace: 'pre-wrap',
-        wordBreak: 'normal',
-        overflowWrap: 'normal',
-      },
-    }
-    const rndProps = {
-      default: this.props.default,
-      dragHandleClassName: '.handle',
-    }
-    return (
-      <Rnd ref={(i) => this.rnd = i} style={s.rnd} {...rndProps}>
-        <div onClick={() => {/* bubbles up */ }} className="handle" style={s.h}>
-          <div>
-            <button type="button" onClick={this.onClickCloseHandler.bind(this)}>close</button>
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      const s: { [key: string]: CSSProperties } = {
+        rnd: {
+          border: '1px silver dashed',
+          backgroundColor: 'lightblue',
+          overflowX: 'hidden',
+          overflowY: 'hidden',
+          zIndex: item.zIndex,
+        },
+        h: {
+          border: '1px lightgray dashed',
+          backgroundColor: 'ghostwhite',
+          display: 'flex',
+        },
+        b: {
+          backgroundColor: 'white',
+          whiteSpace: 'pre-wrap',
+          wordBreak: 'normal',
+          overflowWrap: 'normal',
+        },
+      }
+      const rndProps = {
+        default: item.default,
+        dragHandleClassName: '.handle',
+        onDragStart: item.onBubbleUp,
+        onResizeStart: item.onBubbleUp,
+      }
+      const el = (
+        <Rnd key={item.key}
+          // ref={(i) => this.rnd = i}
+          style={s.rnd} {...rndProps}>
+          <div onClick={item.onBubbleUp} className="handle" style={s.h}>
+            <div>
+              <button type="button" onClick={item.onClose}>close</button>
+            </div>
+            <div>
+              <span>{item.title}</span>
+            </div>
           </div>
-          <div>
-            <span>{this.props.title}</span>
-          </div>
-        </div>
-        <div onClick={() => {/* bubbles up */ }} style={s.b}>{this.props.content}</div>
-      </Rnd>
-    )
-  }
-
-  onClickCloseHandler() {
-    this.props.onClose();
+          <div onClick={item.onBubbleUp} style={s.b}>{item.content}</div>
+        </Rnd >);
+      elList.push(el);
+    }
+    return elList;
   }
 }
